bbee4b7cfb196f26783f899e6ee8b39c98528667
dbrown271 <d.brown271@icloud.com>
2021-11-01 13:34:22 -0500

    super ninja additions

--- oop/superNinjaAssignment/ninja.js
+++ oop/superNinjaAssignment/ninja.js
@@ -12,7 +12,7 @@ class Ninja {
     }
     
     showStats() {
-        let info = `Ninja Stats: \n Name: ${this.name} \n Health: ${this.health} \n Speed: ${this.speed} \n Strength: ${this.strength}`
+        let info = `Ninja Stats: \n Name: ${this.name} \n Health: ${this.health} \n Speed: ${this.speed} \n Strength: ${this.strength} \n Wisdom: ${this.wisdom}`
         console.log(info)
     }
 
@@ -20,8 +20,29 @@ class Ninja {
         this.health += 10
     }
 }
+
+class Sensei extends Ninja{
+    constructor(nameInput, health) {
+        super(nameInput, health)
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+
+    speakWisdom() {
+        let smart = `Wisdom: ${this.wisdom}`
+        console.log("If You Love Something, Let Go. If It Comes Back To You, It's Yours. If It Doesn't, It Never Was.")
+    }
+}
+
 ninja1 = new Ninja("Don Brown", 100)
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+ninja2 = new Sensei("Rob Brown", 200, 10, 10)
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+// ninja1.showStats();
+
+ninja2.showStats();
+ninja2.drinkSake();
+ninja2.speakWisdom()
+ninja2.showStats();
